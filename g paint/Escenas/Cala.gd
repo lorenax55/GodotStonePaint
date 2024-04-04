bcae932770dff039730133512910605cb2397c98
lorenax55 <lorenax230403@gmail.com>
2024-04-04 21:10:05 +0200

    asfsdf

--- g paint/Escenas/Cala.gd
+++ g paint/Escenas/Cala.gd
@@ -4,9 +4,11 @@ var dialog_key = "cala"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	SignalBus.end_dialog.connect(on_end_dialog)
 	SignalBus.emit_signal("display_dialog", dialog_key)
 
-
+func on_end_dialog():
+	get_tree().change_scene_to_file.bind("res://Escenas/Mapa.tscn").call_deferred()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
